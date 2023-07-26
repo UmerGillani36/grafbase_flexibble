072323fae3e3cd7d6228a5a7d6c24cd868f84301
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-26 19:06:42 +0500

    actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,12 @@
 import { GraphQLClient } from "graphql-request";
 
-const makeGraphQLRequest =async(query:string,variables={})=>{
+const isProduction = process.env.NODE_ENV === 'production';
+
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '':'http://'
+
+const client = new GraphQLClient('apiUrl');
+
+const makeGraphQLRequest = async(query:string,variables={})=>{
 try{
     
 } catch(error){
